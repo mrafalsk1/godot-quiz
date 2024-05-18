c2733be9a7e2b99b05d1e02b3b369d5e4eebf772
Matheus Rafalski <matheus__rafalski@hotmail.com>
2024-05-17 21:41:31 -0300

    feat: add searching the questions in json and adding them in the screen

--- Scripts/enums/electives.gd
+++ Scripts/enums/electives.gd
@@ -1,6 +1,34 @@
+
+
 class_name Elective
+var question: String
+var options: Array
+var correct_option_index: int
+
+#func _init(q: String, o: Array[String], coi: int):
+	#print(q)
+	#question = q
+	#options = o
+	#correct_option_index = coi
+
 
 const ELECTIVE_NAMES = {
 	"MATHEMATICS": "Matemática",
 	"PORTUGUESE": "Português"
 }
+
+
+static func get_elective_questions(elective: String, parent_elective) -> Array[Elective]:
+	var json_string = FileAccess.get_file_as_string("res://Database/Questions.json")
+	var questions = JSON.parse_string(json_string)
+	var elective_questions: Array[Elective]
+	for i in questions[parent_elective][elective]:
+		var e = Elective.new()
+		e.question = i["question"]
+		e.options = i["options"]
+		e.correct_option_index = i["correct_option_index"]
+		elective_questions.append(e)
+	return elective_questions
+	
+
+	
--- Scripts/quizz.gd
+++ Scripts/quizz.gd
@@ -1,15 +1,44 @@
 extends Control
 const Electives = preload("res://Scripts/enums/electives.gd")
 
+var current_questions = 0
+@onready var options = $VBoxContainer/Options
+@onready var question = $VBoxContainer/Question
+
+
+
 func _ready():
 	var selected_elective: String = Dialogic.VAR.elective
+	var selected_parent_elective: String = Dialogic.VAR.parent_elective
+	if not selected_elective or not selected_parent_elective:
+		return
+	
 	print("quiz game variavle MUTIO FODA")
 	print(selected_elective)
 	print(Electives.ELECTIVE_NAMES.values())
-	var elective_index =  Electives.ELECTIVE_NAMES.values().find(selected_elective)
-	if elective_index == -1:
-		return
 	
-	var elective_name = Electives.ELECTIVE_NAMES.keys()[Electives.ELECTIVE_NAMES.values().find(selected_elective)]
+	#var elective_index =  Electives.ELECTIVE_NAMES.values().find(selected_elective)
+	#if elective_index == -1:
+		#return
 	
-	print(elective_name)
+	#var elective_name = Electives.ELECTIVE_NAMES.values().find(selected_elective)
+	#var elective = Electives.ELECTIVE_NAMES.keys()[elective_name]
+	load_questions(selected_elective,selected_parent_elective)
+func load_questions(elective: String, parent_elective: String):
+	var questions = Electives.get_elective_questions(elective,parent_elective)
+	change_question(questions[0])
+	print(questions)
+	print(questions[0]["question"])
+		
+#
+#func next_question():
+
+func change_question(elective: Elective):
+	print(elective)
+	question.text = elective.question
+	var buttons = options.get_children()	
+	for idx in range(buttons.size()):
+
+		var b = buttons[idx]
+		b.text = elective.options[idx]
+		print(b.text)
